299916575463702b2d57974b99eef7bb4e4bd3ba
liJIREN <George@guest-wireless-207-151-035-008.usc.edu>
2015-09-03 20:20:53 -0700

    doors change color through levels

---
+++ HexMaze/Assets/_Scripts/CameraManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+	[SerializeField] GameObject character;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position=new Vector3(character.transform.position.x,character.transform.position.y,character.transform.position.z-10f);
+	}
+}
--- HexMaze/Assets/_Scripts/RotationDoor.cs
+++ HexMaze/Assets/_Scripts/RotationDoor.cs
@@ -5,6 +5,10 @@ public class RotationDoor : MonoBehaviour {
     [SerializeField] float speed = 2f;
     [SerializeField] bool isPlayerTouching;
     LTDescr currentRotation;
+    float deltaAngle = 3f;
+    float deltaAngleReverse = 90f;
+    bool isMoved;
+    bool enableMoving = true;
     // Use this for initialization
     void Start () {
     
@@ -12,38 +16,60 @@ public class RotationDoor : MonoBehaviour {
     
     // Update is called once per frame
     void Update () {
-        //if (!isPlayerTouching) 
-        if (transform.eulerAngles.z < 355f && transform.eulerAngles.z > 240f && GetComponent<Rigidbody2D>().angularVelocity!=0f) {
-            if (currentRotation == null) {
-                RotateToAngle(240f);
+        if (enableMoving) {
+            if (transform.eulerAngles.z < 360f - deltaAngle && transform.eulerAngles.z > 240f && GetComponent<Rigidbody2D>().angularVelocity != 0f) {
+                if (currentRotation == null) {
+                    RotateToAngle(240f);
+                    if (transform.eulerAngles.z > 240f+deltaAngleReverse) {
+                        isMoved = true;
+                    }
+                }
             }
-        }
-        if (transform.eulerAngles.z < 235f && transform.eulerAngles.z > 120f && GetComponent<Rigidbody2D>().angularVelocity!=0f) {
-            if (currentRotation == null) {
-                RotateToAngle(120f);
+            if (transform.eulerAngles.z < 240f - deltaAngle && transform.eulerAngles.z > 120f && GetComponent<Rigidbody2D>().angularVelocity != 0f) {
+                if (currentRotation == null) {
+                    RotateToAngle(120f);
+                    if (transform.eulerAngles.z > 1200f+deltaAngleReverse) {
+                        isMoved = true;
+                    }
+                }
             }
-        }
-        if (transform.eulerAngles.z < 115f && transform.eulerAngles.z > 0f && GetComponent<Rigidbody2D>().angularVelocity!=0f) {
-            if (currentRotation == null) {
-                RotateToAngle(0f);
+            if (transform.eulerAngles.z < 120f - deltaAngle && transform.eulerAngles.z > 0f && GetComponent<Rigidbody2D>().angularVelocity != 0f) {
+                if (currentRotation == null) {
+                    RotateToAngle(0f);
+                    if (transform.eulerAngles.z > 0f+deltaAngleReverse) {
+                        isMoved = true;
+                    }
+                }
             }
+        } else {
+            GetComponent<Rigidbody2D>().isKinematic = true;
         }
+        if (Input.GetKeyDown(KeyCode.N)) {
+            NextLevelCheck();
+        }
+    }
+    void NextLevelCheck () {
+        if (isMoved) {
+            SpriteRenderer[] rendererList = new SpriteRenderer[99];
+            rendererList = GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer r in rendererList) {
+                r.color = Color.red;
+            }
+            enableMoving = false;
 
-        
-        
+        }
 
     }
-
     void RotateToAngle (float angle) {
         
         GetComponent<Rigidbody2D>().isKinematic = true;
-        GetComponent<Rigidbody2D>().angularVelocity=0f;
+        GetComponent<Rigidbody2D>().angularVelocity = 0f;
         currentRotation = LeanTween.rotateZ(gameObject, angle, 2f).setOnComplete(() => {
-            Invoke("AllowDoorMovement", 0.5f);
+                Invoke("AllowDoorMovement", 0.5f);
             });
     }
 
-    void AllowDoorMovement() {
+    void AllowDoorMovement () {
         GetComponent<Rigidbody2D>().isKinematic = false;
         currentRotation = null;
     }
